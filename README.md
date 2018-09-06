993d4ea89a78b6060671e017a2eab9615529263a
Anatoly Bardukov <sindbag@gmail.com>
2018-09-06 19:55:32 +0300

    Receipts bugfixes and readme update

--- README.md
+++ README.md
@@ -15,3 +15,9 @@
 1. Get block data from public ETH node by `src/block_export` to RethinkDB.
 2. Get block's transactions data by `src/tx_export` by pub/sub and web3.
 3. Get transaction's receipt data by `src/tx_receipt_export` by pub/sub and web3.
+
+## Setup and run
+
+    yarn
+    docker-compose up -d # run rethinkdb
+    yarn start-all       # buggy
--- src/tx_receipt_export.ts
+++ src/tx_receipt_export.ts
@@ -4,23 +4,23 @@ import {web3, RDB_NODE, DB_NAME, TABLE_TX_RECEIPTS, TABLE_TXS} from './config'
 
 async function syncTxReceipts(conn: r.Connection, db: r.Db, txsTable: string, receiptsTable: string) {
     let txs = await DB.getPendingReceipts(conn, db, txsTable, receiptsTable)
-    txs.each(async (err: any, hash: string) => {
+    txs.each(async (err: any, tx: any) => { // tx is tx object
         if (err) console.error(err)
-        console.log('Syncing receipt for tx: ', hash)
+        console.log('Syncing receipt for tx: ', tx.hash)
         let receipt;
         try {
-            receipt = await web3.eth.getTransactionReceipt(hash)
+            receipt = await web3.eth.getTransactionReceipt(tx.hash)
         } catch (err) {
-            console.error('Failed to fetch receipt for transaction', hash, err)
+            console.error('Failed to fetch receipt for transaction', tx.hash, err)
         }
         if (receipt) {
             // replace `transactionHash` with `hash` for uniformity
-            const txRec = {...receipt, hash}
+            const txRec = {...receipt, hash: tx.hash}
             delete txRec.transactionHash
             console.log('Got transaction:', txRec.hash)
             DB.insert(conn, db, receiptsTable, txRec)
         } else {
-            console.error('Transaction receipt is empty: ', hash)
+            console.error('Transaction receipt is empty: ', tx.hash)
         }
     })
 }
